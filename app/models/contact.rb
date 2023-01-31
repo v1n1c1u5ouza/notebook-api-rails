5107434711abaabd8cdf9deb828cb1b22e992ebd
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-01-31 13:51:01 -0300

    Feat: JSON render

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,9 +4,15 @@ class Contact < ApplicationRecord
     def author
         "v1n1c1u5ouza"
     end
-    
+
+    def kind_description
+        self.kind.description
+    end 
+        
     def as_json(options={})
-        super(methods: :author, root: true)
+        super(
+            root: true, methods: [:kind_description, :author], include: { kind: { only: :description }}
+        )
     end
       
 end
